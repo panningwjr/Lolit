5c0951d64ecd32c3f339f5563bfd623d7a8f69f0
Tomcat <panningwjr@hotmail.com>
2013-04-09 21:49:34 +0800

    4.9

--- src/dao/impl/MemberImpl.java
+++ src/dao/impl/MemberImpl.java
@@ -43,14 +43,23 @@ public class MemberImpl implements MemberDAO {
 		String mName = member.getmAcName();
 		String mPwd = member.getmPwd();
 		int mRole = member.getmRole();
+		System.out.println(mRole);
 		String sql = "";
 
 		try {
-			sql = "SELECT mPwd FROM lolit.member WHERE mAcName = '" + mName
-					+ "' AND mRole <= '" + mRole + "'";
+			// 判断是否验证管理员或者超级管理员权限,赛组委
+			if (mRole <= 2) {
+				sql = "SELECT mPwd FROM lolit.member WHERE mAcName = '" + mName
+						+ "' AND mRole <= '" + mRole + "'";
+			} else {
+				//验证营销官,传播人员登录,管理员和超级管理员可以登录
+				sql = "SELECT mPwd FROM lolit.member WHERE mAcName = '" + mName
+						+ "' AND (mRole = '" + mRole + "' or mRole < '2')";
+			}
+			System.out.println(sql);
 			pstmt = conn.prepareStatement(sql);
 			rs = pstmt.executeQuery();
-			if (rs.next()) {
+			while(rs.next()) {
 				if (rs.getString("mPwd").equals(mPwd)) {
 					return true;
 				}
